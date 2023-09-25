eb657f345d7e16189ed05adb9b9ffb7052e677c0
dev-oe <deliicmugambi123@gmail.com>
2023-09-25 16:57:19 +0000

    Closes: #1
    Updated validations.py script.
    
    Added additional conditional checks to validate the first character
    doesn't start with either of the forbidded character.

--- Course3/Lab4/validations.py
+++ Course3/Lab4/validations.py
@@ -1,24 +1,44 @@
-#!/usr/bin/env python3
-
 import re
 
 def validate_user(username, minlen):
-    """Checks if the received username matches the required conditions."""
-    if type(username) != str:
-        raise TypeError("username must be a string")
+    """
+    Checks if the received username matches the required conditions.
+
+    Args:
+        username (str): The username to validate.
+        minlen (int): The minimum length allowed for the username.
+
+    Returns:
+        bool: True if the username is valid, False otherwise.
+    """
+    # Check if username is a string
+    if not isinstance(username, str):
+        raise TypeError("Username must be a string")
+
+    # Check if minlen is at least 1
     if minlen < 1:
-        raise ValueError("minlen must be at least 1")
-    
+        raise ValueError("Minimum length must be at least 1")
+
     # Usernames can't be shorter than minlen
     if len(username) < minlen:
         return False
-    # Usernames can only use letters, numbers, dots and underscores
+
+    # Usernames can only use letters, numbers, dots, and underscores
     if not re.match('^[a-z0-9._]*$', username):
         return False
+
     # Usernames can't begin with a number
     if username[0].isnumeric():
         return False
+
+    # Additional check: Username can't start with forbidden characters
+    forbidden_chars = ['.', '_']  # Add any other forbidden characters as needed
+    if username[0] in forbidden_chars:
+        return False
+
+    # If all conditions are met, the username is valid
     return True
-
-
-
+print(validate_user("blue.kale", 3)) # True
+print(validate_user(".blue.kale", 3)) # Currently True, should be False
+print(validate_user("red_quinoa", 4)) # True
+print(validate_user("_red_quinoa", 4)) # Currently True, should be False
